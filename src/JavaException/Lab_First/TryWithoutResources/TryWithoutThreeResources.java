33eb28294e54cd1011970d91867d5783573900f5
Mishelder <mi3h1@mail.ru>
2020-05-21 13:37:35 +0300

    add solution try without three resources on Java 6

---
+++ src/JavaException/Lab_First/TryWithoutResources/TryWithoutThreeResources.java
@@ -0,0 +1,118 @@
+package JavaException.Lab_First.TryWithoutResources;
+
+import JavaException.Lab_First.AutoCloseableFactory;
+import JavaException.Lab_First.TryBody;
+
+public class TryWithoutThreeResources {
+
+    public static void call(AutoCloseableFactory factoryA,
+                            AutoCloseableFactory factoryB,
+                            AutoCloseableFactory factoryC,
+                            TryBody body) throws Throwable{
+        //create A
+        AutoCloseable resourceA = factoryA.create();
+        AutoCloseable resourceB;
+        AutoCloseable resourceC;
+
+        //create B
+        try{
+            resourceB = factoryB.create();
+        }catch (Throwable createBEx){
+            try{
+                resourceA.close();
+            }catch (Throwable closeAEx){
+                createBEx.addSuppressed(closeAEx);
+            }
+            throw createBEx;
+        }
+
+        //create C
+        try{
+            resourceC = factoryC.create();
+        }catch (Throwable createCEx){
+            try{
+                resourceB.close();
+            }catch (Throwable closeBEx){
+                createCEx.addSuppressed(closeBEx);
+                try{
+                    resourceA.close();
+                }catch (Throwable closeAEx){
+                    createCEx.addSuppressed(closeAEx);
+                }
+            }
+            throw createCEx;
+        }
+
+        //tryBody
+        try{
+            body.runBody();
+        }catch(Throwable bodyEx){
+            //close C
+            try{
+                resourceC.close();
+            }catch (Throwable closeCEx){
+                bodyEx.addSuppressed(closeCEx);
+                try{
+                    resourceB.close();
+                }catch (Throwable closeBEx){
+                    bodyEx.addSuppressed(closeBEx);
+                    try{
+                        resourceA.close();
+                    }catch (Throwable closeAEx){
+                        bodyEx.addSuppressed(closeAEx);
+                    }
+                }
+            }
+            //close B
+            try{
+                resourceB.close();
+            }catch (Throwable closeBEx){
+                bodyEx.addSuppressed(closeBEx);
+                try{
+                    resourceA.close();
+                }catch (Throwable closeAEx){
+                    bodyEx.addSuppressed(closeAEx);
+                }
+            }
+
+            //close A
+            try{
+                resourceA.close();
+            }catch (Throwable closeAEx){
+                    bodyEx.addSuppressed(closeAEx);
+            }
+            throw bodyEx;
+        }
+
+        //close C
+        try{
+            resourceC.close();
+        }catch (Throwable closeCEx){
+            try{
+                resourceB.close();
+            }catch (Throwable closeBEx){
+                closeCEx.addSuppressed(closeBEx);
+                try{
+                    resourceA.close();
+                }catch (Throwable closeAEx){
+                    closeCEx.addSuppressed(closeAEx);
+                }
+            }
+            throw closeCEx;
+        }
+        //close B
+        try{
+            resourceB.close();
+        }catch (Throwable closeBEx){
+            try{
+                resourceA.close();
+            }catch (Throwable closeAEx){
+                closeBEx.addSuppressed(closeAEx);
+            }
+            throw closeBEx;
+        }
+        //close A;
+        resourceA.close();
+
+    }
+}
